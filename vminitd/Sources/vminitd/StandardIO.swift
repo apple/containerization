fbdb8b072b2d901e5c41f6021064a9555c4d8117
Dmitry Kovba <dkovba@apple.com>
2025-10-24 11:08:14 -0700

    Fix a compilation error

--- vminitd/Sources/vminitd/StandardIO.swift
+++ vminitd/Sources/vminitd/StandardIO.swift
@@ -14,6 +14,7 @@
 // limitations under the License.
 //===----------------------------------------------------------------------===//
 
+import ContainerizationError
 import ContainerizationOS
 import Foundation
 import Logging
