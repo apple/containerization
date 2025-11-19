d0da3717273d24baa72e7b8cd4e0be66692c39d0
Danny Canter <danny_canter@apple.com>
2025-11-19 03:13:34 -0800

    Vminitd: Add pause command
    Due to us supporting a pod type now, and pid ns sharing
    being quite a common thing for pods, lets add a pause container
    like command to vminitd to eventually enable pid ns sharing between
    containers in our variant of a pod.
    
    This changes vminitd slightly to have pause and init (default)
    commands as it seemed simpler than creating a whole new binary
    to include in the guest image.

--- vminitd/Sources/vminitd/Application.swift
+++ vminitd/Sources/vminitd/Application.swift
@@ -14,53 +14,15 @@
 // limitations under the License.
 //===----------------------------------------------------------------------===//
 
-import Containerization
-import ContainerizationError
-import ContainerizationOS
 import Foundation
 import Logging
-import NIOCore
-import NIOPosix
-
-#if os(Linux)
-import Musl
-import LCShim
-#endif
 
 @main
 struct Application {
-    private static let foregroundEnvVar = "FOREGROUND"
-    private static let vsockPort = 1024
-    private static let standardErrorLock = NSLock()
-
-    private static func runInForeground(_ log: Logger) throws {
-        log.info("running vminitd under pid1")
-
-        var command = Command("/sbin/vminitd")
-        command.attrs = .init(setsid: true)
-        command.stdin = .standardInput
-        command.stdout = .standardOutput
-        command.stderr = .standardError
-        command.environment = ["\(foregroundEnvVar)=1"]
-
-        try command.start()
-        _ = try command.wait()
-    }
-
-    private static func adjustLimits() throws {
-        var limits = rlimit()
-        guard getrlimit(RLIMIT_NOFILE, &limits) == 0 else {
-            throw POSIXError(.init(rawValue: errno)!)
-        }
-        limits.rlim_cur = 65536
-        limits.rlim_max = 65536
-        guard setrlimit(RLIMIT_NOFILE, &limits) == 0 else {
-            throw POSIXError(.init(rawValue: errno)!)
-        }
-    }
+    static let standardErrorLock = NSLock()
 
     @Sendable
-    private static func standardError(label: String) -> StreamLogHandler {
+    static func standardError(label: String) -> StreamLogHandler {
         standardErrorLock.withLock {
             StreamLogHandler.standardError(label: label)
         }
@@ -68,65 +30,18 @@ struct Application {
 
     static func main() async throws {
         LoggingSystem.bootstrap(standardError)
-        var log = Logger(label: "vminitd")
 
-        try adjustLimits()
+        // Parse command line arguments
+        let args = CommandLine.arguments
+        let command = args.count > 1 ? args[1] : "init"
 
-        // when running under debug mode, launch vminitd as a sub process of pid1
-        // so that we get a chance to collect better logs and errors before pid1 exists
-        // and the kernel panics.
-        #if DEBUG
-        let environment = ProcessInfo.processInfo.environment
-        let foreground = environment[Self.foregroundEnvVar]
-        log.info("checking for shim var \(foregroundEnvVar)=\(String(describing: foreground))")
-
-        if foreground == nil {
-            try runInForeground(log)
-            exit(0)
-        }
-
-        // since we are not running as pid1 in this mode we must set ourselves
-        // as a subpreaper so that all child processes are reaped by us and not
-        // passed onto our parent.
-        CZ_set_sub_reaper()
-        #endif
-
-        signal(SIGPIPE, SIG_IGN)
-
-        // Because the sysctl rpc wouldn't make sense if this didn't always exist, we
-        // ALWAYS mount /proc.
-        guard Musl.mount("proc", "/proc", "proc", 0, "") == 0 else {
-            log.error("failed to mount /proc")
-            exit(1)
-        }
-        guard Musl.mount("tmpfs", "/run", "tmpfs", 0, "") == 0 else {
-            log.error("failed to mount /run")
-            exit(1)
-        }
-        try Binfmt.mount()
-
-        log.logLevel = .debug
-
-        log.info("vminitd booting")
-        let eg = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
-        let server = Initd(log: log, group: eg)
-
-        do {
-            log.info("serving vminitd API")
-            try await server.serve(port: vsockPort)
-            log.info("vminitd API returned, syncing filesystems")
-
-            #if os(Linux)
-            Musl.sync()
-            #endif
-        } catch {
-            log.error("vminitd boot error \(error)")
-
-            #if os(Linux)
-            Musl.sync()
-            #endif
-
-            exit(1)
+        switch command {
+        case "pause":
+            try PauseCommand.run()
+        case "init":
+            try await InitCommand.run()
+        default:
+            try await InitCommand.run()
         }
     }
 }
