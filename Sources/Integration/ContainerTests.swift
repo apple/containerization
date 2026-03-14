9644e61a65906fa154c141bfa55c1e14d7b314d2
Danny Canter <danny_canter@apple.com>
2026-03-14 01:32:28 -0700

    Network: Move to standalone type
    The network protocol and VMNetNetwork implementation currently are
    housed on ContainerManager even though they are generally useful
    types even outside of this easy to use helper type. This change moves
    them to not be nested types anymore, as well as exposes a new param
    on VMNetNetworks constructor so we can pass the type of network.

--- Sources/Integration/ContainerTests.swift
+++ Sources/Integration/ContainerTests.swift
@@ -2895,12 +2895,12 @@ extension IntegrationSuite {
         let bs = try await bootstrap(id)
 
         let customMTU: UInt32 = 1400
-        var network = try ContainerManager.VmnetNetwork()
+        var network = try VmnetNetwork()
         defer {
-            try? network.release(id)
+            try? network.releaseInterface(id)
         }
 
-        guard let interface = try network.create(id, mtu: customMTU) else {
+        guard let interface = try network.createInterface(id, mtu: customMTU) else {
             throw IntegrationError.assert(msg: "failed to create network interface")
         }
 
@@ -3974,7 +3974,7 @@ extension IntegrationSuite {
         let id = "test-networking-disabled"
         let bs = try await bootstrap(id)
 
-        let network = try ContainerManager.VmnetNetwork()
+        let network = try VmnetNetwork()
         var manager = try ContainerManager(vmm: bs.vmm, network: network)
         defer {
             try? manager.delete(id)
@@ -4027,7 +4027,7 @@ extension IntegrationSuite {
         let id = "test-networking-enabled"
         let bs = try await bootstrap(id)
 
-        let network = try ContainerManager.VmnetNetwork()
+        let network = try VmnetNetwork()
         var manager = try ContainerManager(vmm: bs.vmm, network: network)
         defer {
             try? manager.delete(id)
