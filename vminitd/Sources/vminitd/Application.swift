1da3511ea0565dfe0beba21bc71d64ee3a2c3aa3
michael_crosby <michael_crosby@apple.com>
2026-03-17 15:20:09 -0400

    Revert "Upgrade vminitd API to use grpc-swift-2. (#578)"
    This reverts commit 7ffe6d2ed54569388581ea4aad6787edbbd9aa90.

--- vminitd/Sources/vminitd/Application.swift
+++ vminitd/Sources/vminitd/Application.swift
@@ -117,45 +117,8 @@ struct LogLevelOption: ParsableArguments {
 }
 
 func makeLogger(label: String, level: Logger.Level) -> Logger {
-    LoggingSystem.bootstrap { label in StderrLogHandler(label: label) }
+    LoggingSystem.bootstrap(StreamLogHandler.standardError)
     var log = Logger(label: label)
     log.logLevel = level
     return log
-}
-
-private struct StderrLogHandler: LogHandler {
-    let label: String
-    var logLevel: Logger.Level = .info
-    var metadata: Logger.Metadata = [:]
-
-    subscript(metadataKey key: String) -> Logger.Metadata.Value? {
-        get { metadata[key] }
-        set { metadata[key] = newValue }
-    }
-
-    func log(
-        level: Logger.Level, message: Logger.Message, metadata: Logger.Metadata?,
-        source: String, file: String, function: String, line: UInt
-    ) {
-        var merged = self.metadata
-        metadata?.forEach { merged[$0] = $1 }
-        let metaStr = merged.isEmpty ? "" : " \(merged.map { "\($0): \($1)" }.sorted().joined(separator: ", "))"
-        let ts = isoTimestamp()
-        let data = "\(ts) \(level) \(label):\(metaStr) \(message)\n".data(using: .utf8) ?? Data()
-        FileHandle.standardError.write(data)
-    }
-
-    func isoTimestamp() -> String {
-        let date = Date()
-        var time = time_t(date.timeIntervalSince1970)
-        var ms = Int(date.timeIntervalSince1970 * 1000) % 1000
-        if ms < 0 { ms += 1000 }
-        var tm = tm()
-        gmtime_r(&time, &tm)
-        let buf = withUnsafeTemporaryAllocation(of: CChar.self, capacity: 32) { ptr -> String in
-            strftime(ptr.baseAddress!, 32, "%Y-%m-%dT%H:%M:%S", &tm)
-            return String(cString: ptr.baseAddress!)
-        }
-        return String(format: "%@.%03dZ", buf, ms)
-    }
 }
