4ba3ffc8154a5af734a8bc6356fe272b9afc175b
Dmitry Kovba <dkovba@apple.com>
2026-03-30 13:35:20 -0700

    Fix a hardlink count decrement threshold

--- Sources/ContainerizationEXT4/EXT4+Formatter.swift
+++ Sources/ContainerizationEXT4/EXT4+Formatter.swift
@@ -235,7 +235,7 @@ extension EXT4 {
                 // the file we are deleting is a hardlink, decrement the link count
                 let linkedInodePtr = self.inodes[Int(hardlink - 1)]
                 var linkedInode = linkedInodePtr.pointee
-                if linkedInode.linksCount > 2 {
+                if linkedInode.linksCount > 1 {
                     linkedInode.linksCount -= 1
                     linkedInodePtr.initialize(to: linkedInode)
                 }
