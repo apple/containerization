89cf6f0a3e946250f20d0e050b6a078dfa0999b9
Dmitry Kovba <dkovba@apple.com>
2026-04-06 19:17:06 -0700

    Fix for the last `xattr` dropped at the buffer boundary

--- Sources/ContainerizationEXT4/EXT4+Xattrs.swift
+++ Sources/ContainerizationEXT4/EXT4+Xattrs.swift
@@ -257,13 +257,13 @@ extension EXT4 {
             var i = start
             var attribs: [ExtendedAttribute] = []
             // 16 is the size of 1 XAttrEntry
-            while i + 16 < buffer.count {
+            while i + 16 <= buffer.count {
                 let attributeStart = i
                 let rawXattrEntry = Array(buffer[i..<i + 16])
                 let xattrEntry = try EXT4.XAttrEntry(using: rawXattrEntry)
                 i += 16
                 var endIndex = i + Int(xattrEntry.nameLength)
-                guard endIndex < buffer.count else {
+                guard endIndex <= buffer.count else {
                     continue
                 }
                 let rawName = buffer[i..<endIndex]
